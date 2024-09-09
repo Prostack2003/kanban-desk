1792fad767c43a049ea4f5e4d9b6ec83cdc0a420
Vladimir <cool.deds@mail.ru>
2024-09-09 15:16:08 +0300

    fix: reinstall eslint and correct scripts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,30 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [new ESLintPlugin({
-        extensions: ['ts', 'js', 'jsx', 'tsx'],
-        fix: true,
-    })],
+module.exports = (env: any) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'build'),
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        plugins: [new ESLintPlugin({
+            extensions: ['ts', 'js', 'jsx', 'tsx'],
+            fix: true,
+        })],
+    }
 };
