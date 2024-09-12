2c715a18b0e4395992c8607214b653685fe450b8
Vladimir <cool.deds@mail.ru>
2024-09-12 16:49:18 +0300

    feat: init react structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,12 @@
-console.log('Hello world, 2')
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.scss';
+import App from './App';
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
