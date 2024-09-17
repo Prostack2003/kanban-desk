56a1c0e32ca7b5391e0f9db95304a512720df8a9
Vladimir <cool.deds@mail.ru>
2024-09-17 15:09:18 +0300

    feat: add tooltip logic

--- src/components/BoardCard/BoardCard.styles.ts
+++ src/components/BoardCard/BoardCard.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -8,4 +9,4 @@ export const Wrapper = styled.div`
     background-color: white;
     padding-block: 200px;
     padding-inline: 150px;
-    `
+`
--- src/components/BoardCard/BoardCard.tsx
+++ src/components/BoardCard/BoardCard.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { Wrapper } from './BoardCard.styles';
 import { Button } from '../../elements/Buttons/MainButton/Button.styles';
+import { TooltipButton, TooltipWrapper } from '../../elements/Buttons/Tooltip/Tooltip.styles';
+import ChangeModal from '../ChangeModal/ChangeModal';
+import DeleteModal from '../DeleteModel/DeleteModal';
 
 interface BoardCardProps {
   name: string,
-  key: number
+  id: number,
+  onDelete: () => void;
+  setDesks: Dispatch<SetStateAction<{ id: number; name: string; }[]>>;
 }
 
-export default function BoardCard({ name, key }: BoardCardProps) {
+export default function BoardCard({ name, id, onDelete, setDesks }: BoardCardProps) {
+  const [isOpenTooltip, setIsOpenTooltip] = useState(false);
+
+  const [isChangeModalOpen, setIsChangeModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const openTooltip = () => setIsOpenTooltip(true);
+  const closeTooltip = () => setIsOpenTooltip(false);
+
+  const handleEdit = () => {
+    closeTooltip()
+    setIsChangeModalOpen(true)
+  };
+
+  const handleDelete = () => {
+    closeTooltip()
+    setIsDeleteModalOpen(true)
+  };
+
+  const confirmDelete = () => {
+    onDelete()
+    setIsDeleteModalOpen(false)
+  }
+
   return (
     <Wrapper>
-      <p key={key}>{name}</p>
-      <Button>Изменить Доску</Button>
+      <p key={id}>{name}</p>
+      <Button onClick={openTooltip}>Изменить Доску</Button>
+      {isOpenTooltip && (
+        <TooltipWrapper>
+          <TooltipButton onClick={handleEdit}>Редактировать</TooltipButton>
+          <TooltipButton onClick={handleDelete}>Удалить</TooltipButton>
+        </TooltipWrapper>
+      )
+      }
+      <ChangeModal
+        isOpenTooltip={isChangeModalOpen}
+        onClose={() => setIsChangeModalOpen(false)}
+        name={name}
+        deskId={id}
+        setDesks={setDesks}
+      />
+      <DeleteModal
+        isOpenTooltip={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onDelete={confirmDelete}
+      />
     </Wrapper>
   );
 }
