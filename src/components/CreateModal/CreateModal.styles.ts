f95c8718d21a3c3139882030d0e6d65b911344b8
Vladimir <cool.deds@mail.ru>
2024-09-17 15:13:18 +0300

    feat: decompozition styles

--- src/components/CreateModal/CreateModal.styles.ts
+++ src/components/CreateModal/CreateModal.styles.ts
@@ -1,36 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    gap: 10px;
-    align-items: center;
-    `
-
-export const ModalOverlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-  export const ModalContent = styled.div`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 20px;
-      color: white;
-      border-radius: 8px;
-      max-width: 500px;
-      width: 100%;
-      background-color: #282c34;
-  `
-
 export const InputModal = styled.input`
     margin-block: 10px;
     padding: 8px;
