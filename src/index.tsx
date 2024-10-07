5a1a335272954f375097e063e1e3af02887b0236
Vladimir <cool.deds@mail.ru>
2024-10-07 17:26:31 +0300

    feat: remove strict mode in react for dnd

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,5 @@ import { router } from './routes/router';
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 root.render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
 )
